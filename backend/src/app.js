d7a80078953f521fe6d5c733074afa2539355fac
Anshuman Gogoi <116825724+cyberhypex@users.noreply.github.com>
2026-05-22 12:46:40 +0530

    Logger

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,12 @@
 const express=require("express");
 const cors=require("cors");
 const app=express();
+const connectDB=require("./config/dbConfig")
 app.use(cors());
 app.use(express.json());
 module.exports=app;
+connectDB();
+
+app.get("/",(req,res)=>{
+    res.json({message:`Welcome to Urban Threat Intelligence API`})
+})
